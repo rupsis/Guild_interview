14fc077b43d891e69a12cc664eca60a495d9ca32
Nate Rupsis <nathaniel.rupsis@homeadvisor.com>
2021-01-04 17:30:07 -0700

    add db seed

--- db/db_seed.rb
+++ db/db_seed.rb
@@ -5,13 +5,26 @@ require 'pg'
 conn = PG.connect('localhost', '5555', nil, nil, 'guild', 'postgres', 'test')
 
 
+conn.prepare('users', 'insert into "user" (firstName, lastName) values ($1, $2)')
+conn.prepare('messages', 'insert into messages (senderId, receiverId, message, "timestamp") values ($1, $2, $3, $4)')
 
 # Create 100 users
+(0..100).step(1) do |n|
+    conn.exec_prepared('users', [ Faker::Name.first_name, Faker::Name.last_name ])
+end
 
 
+# Create 10,000 messages
+(0..10000).step(1) do |n|
+    conn.exec_prepared('messages', [ 
+        Faker::Number.between(from: 1, to: 100),
+        Faker::Number.between(from: 1, to: 100),
+        Faker::Lorem.paragraph,
+        Faker::Time.between(from: DateTime.now - 100, to: DateTime.now)
+    ])
+end
 
 
-# Create a 100,000 messages to random users
 
 
 
